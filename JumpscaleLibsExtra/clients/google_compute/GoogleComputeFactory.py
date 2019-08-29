34ea2dfd529f1ee92bc3b0ca8863a687ccda910e
Waleed Hammam <waleed.hammam@gmail.com>
2019-08-29 16:05:26 +0200

    add .jumpscalemodules file to add tfgateway

--- JumpscaleLibsExtra/clients/google_compute/GoogleComputeFactory.py
+++
@@ -1,9 +0,0 @@
-from .GoogleCompute import GoogleCompute
-from Jumpscale import j
-
-JSBASE = j.baseclasses.object_config_collection
-
-
-class GoogleComputeFactory(JSBASE):
-    __jslocation__ = "j.clients.google_compute"
-    _CHILDCLASS = GoogleCompute
