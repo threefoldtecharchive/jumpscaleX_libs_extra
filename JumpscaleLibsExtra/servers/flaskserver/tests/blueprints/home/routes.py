a489ed6b62a8ec9272971fc1d236e79eee026a89
serboctor <boctor.sarah@gmail.com>
2019-10-13 12:44:03 +0200

    Remove sanic and flask servers

--- JumpscaleLibsExtra/servers/flaskserver/tests/blueprints/home/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from blueprints.home import blueprint
-from Jumpscale import j
-
-
-@blueprint.route("/", methods=["GET"])
-def route_index():
-    # j.shell()
-    return render_template("index.html")
