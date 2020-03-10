15df6bdbd427a18dba5c3bba13ed7cc8422ade49
Reem Khamis <khamisr@codescalers.com>
2020-03-10 17:30:55 +0200

    add jslocation to taiga

--- JumpscaleLibsExtra/clients/taiga/TaigaFactory.py
+++ JumpscaleLibsExtra/clients/taiga/TaigaFactory.py
@@ -7,6 +7,7 @@ JSConfigs = j.baseclasses.object_config_collection
 class TaigaFactory(JSConfigs):
     # check https://github.com/threefoldtech/jumpscaleX_libs_extra/issues/7
     _CHILDCLASS = TaigaClient
+    __jslocation__ = "j.clients.taiga"
 
     def install(self, reset=False):
         j.builders.runtimes.pip.install("python-taiga", reset=reset)
