b48c205a1db50107fe0119769e45cf6578794ded
Waleed <waleed.hammam@gmail.com>
2020-04-09 21:41:05 +0200

    Unstable identity2 (#27)
    * simulators
    
    * simulator, improvements
    
    * identity manager
    
    * simulator
    
    * simulator
    
    * simulator update
    
    * simulator with parameters
    
    * simulator many improvements
    
    * complete refactoring j.tools.threebot to j.me.encryptor.tools
    
    * fix jslocation in threebot deploy
    
    * Revert "fix jslocation in threebot deploy"
    
    This reverts commit b92009c71979a0ed008c69121b43b9223a703312.
    
    * Revert "complete refactoring j.tools.threebot to j.me.encryptor.tools"
    
    This reverts commit 75843fe57e4b75884c36c6fed33bc2bcf12bcc3a.
    
    Co-authored-by: kristof <kristof@incubaid.com>
    Co-authored-by: m-motawea <magedmotawea@gmail.com>

--- JumpscaleLibsExtra/tools/threefold_simulation/notebooks/hardware/A_dc_rack.py
+++ JumpscaleLibsExtra/tools/threefold_simulation/notebooks/hardware/A_dc_rack.py
@@ -1,21 +1,19 @@
 from Jumpscale import j
 
-#as used in gec farms as standard rack
+# as used in gec farms as standard rack
 
-def bom_calc(bom, environment):
 
-    from hardware.components_hpe import bom_populate
-    bom=bom_populate(bom)
+def bom_calc(environment):
 
+    from hardware.components.components_hpe import bom_populate
+
+    environment.bom = bom_populate(environment.bom)
     # see the bill of material sheet to define the devices
-    compute = bom.device_get("hpe_compute_server", device_template_name="hpe_compute_server", environment=environment)
-    storage = bom.device_get("hpe_storage_server", device_template_name="hpe_storage_server", environment=environment)
-    switch = bom.device_get("switch_48", device_template_name="switch_48", environment=environment)
+    compute = environment.bom.device_get("hpe_compute_server")
+    storage = environment.bom.device_get("hpe_storage_server")
+    switch = environment.bom.device_get("switch_48")
 
     # an environment to simulate the overhead per node (eg. 1 switch per node)
     environment.device_node_add("compute", compute, 11)
     environment.device_node_add("storage", storage, 5)
     environment.device_overhead_add("switch", switch, 2)
-
-    return bom, environment
-
--- JumpscaleLibsExtra/tools/threefold_simulation/notebooks/simulations/default.py
+++ JumpscaleLibsExtra/tools/threefold_simulation/notebooks/simulations/default.py
@@ -2,15 +2,7 @@
 from Jumpscale import j
 
 
-def simulation_calc(simulation, environment, bom):
-
-    # sales parameters
-    # these are enduser prices for people buying capacity
-    # following prices are very aggressive
-    # compute unit = 4 GB memory and 2 virtual CPU, in market price between 40 and USD120
-    simulation.sales_price_cu = "8 USD"
-    # storage unit = 1 TB of netto usable storage, in market prices between 20 and USD120
-    simulation.sales_price_su = "5 USD"
+def simulation_calc(simulation, environment):
 
     # costs for environment
     # is the cost of 1 kwh
@@ -29,31 +21,10 @@ def simulation_calc(simulation, environment, bom):
     # utilization of the nodes, starting with 0, after 20 months starting March 2020 we go to 80% utilization
     simulation.utilization_set("20:80,40:90")
 
-    # super important factor, how does token price goes up
-    # this is ofcourse complete speculation, no-one knows
-    simulation.tokenprice_set_5years(3)
-
-    ##if you want to do it more specific
-    # 0:0 means month 0 we have value 0
-    # 60:3 means month 60: value is 3
-    # interpolation will happen between the values
-    # so below will let the price go from 0.15 first month to 3 over 60 months
-    # 119 is the last month we need to set (10 years from now, the relevant one is 5 years but simulator needs a further one
-    # simulation.tokenprice_set("0:0.15,60:3,119:6")
-
-    # month:growth_percent of nodes being added
-    # this means month 1 (is 0 in this file) we add 5 nodes, month 12 we add 1000 new nodes
-
-    simulation.nrnodes_new_set("0:5,6:150,12:1000,18:2000,24:8000,36:12000,48:20000,60:20000")
-    # simulation.nrnodes_new_set("0:5,6:150,20:5000")
-
     # first batch of nodes added is 1500 nodes
     # each node is +-4.5k usd (check the bill of material sheet)
     # and we start the simulation with 800m tokens already farmed by the TF Farmers
     simulation.nodesbatch_start_set(
-        environment=environment, nrnodes=1500, months_left=36, tft_farmed_before_simulation=800 * 1000 * 1000
+        environment=environment, nrnodes=1500, months_left=36, tft_farmed_before_simulation=700 * 1000 * 1000
     )
-    # we can get the simulator to add the batches automatically based on chosen environment
-    simulation.nodesbatches_add_auto(environment=environment)
-
-    return (simulation, environment, bom)
+    return (simulation, environment)
