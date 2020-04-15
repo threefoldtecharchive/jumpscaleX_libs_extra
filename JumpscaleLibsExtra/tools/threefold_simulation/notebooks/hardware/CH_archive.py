d63f0566f8cc7ebb89ffc4a423b965ec09ee5934
kristof <kristof@incubaid.com>
2020-04-15 15:58:48 +0200

    ch_archive simulator

--- JumpscaleLibsExtra/tools/threefold_simulation/notebooks/hardware/CH_archive.py
+++ JumpscaleLibsExtra/tools/threefold_simulation/notebooks/hardware/CH_archive.py
@@ -10,11 +10,11 @@ def bom_calc(environment):
     environment.bom = bom_populate(environment.bom)
 
     # see the bill of material sheet to define the devices
-    compute = environment.bom.device_get("archive_compute_frontend", environment=environment)
-    storage = environment.bom.device_get("storage_server", environment=environment)
-    switch = environment.bom.device_get("switch_48", environment=environment)
+    # compute = environment.bom.device_get("archive_compute_frontend", environment=environment)
+    # storage = environment.bom.device_get("storage_server", environment=environment)
+    # switch = environment.bom.device_get("switch_48", environment=environment)
 
     # an environment to simulate the overhead per node (eg. 1 switch per node)
-    environment.device_node_add("compute", compute, 29)
-    environment.device_node_add("storage", storage, 285)
-    environment.device_overhead_add("switch", switch, 30)
+    environment.device_node_add("compute", template="archive_compute_frontend", nr=29)
+    environment.device_node_add("storage", template="storage_server", nr=285)
+    environment.device_overhead_add("switch", template="switch_48", nr=30)
