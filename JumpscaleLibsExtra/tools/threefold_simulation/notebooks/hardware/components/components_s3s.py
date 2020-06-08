d2f6a0755e8c3b020e3085d16b5673d5e6ee62d4
Andreas Hartl <andreas@greenitglobe.com>
2020-06-08 18:49:02 +0200

    updated to latest configs

--- JumpscaleLibsExtra/tools/threefold_simulation/notebooks/hardware/components/components_s3s.py
+++ JumpscaleLibsExtra/tools/threefold_simulation/notebooks/hardware/components/components_s3s.py
@@ -2,22 +2,22 @@ def bom_populate(bom):
     
     bom.components.new(
         name="s1",
-        description="SN1 Chassis sm tower, gigabyte motherboard, eur pricing",
-        cost=414,
+        description="Stingy Bee Small all in one box eur pricing",
+        cost=499,
         rackspace_u=0,
         cru=0,
-        sru=0,
+        sru=256,
         hru=0,
-        mru=0,
+        mru=8,
         su_perc=50,
         cu_perc=50,
-        power=150,
+        power=120,
     )
     
     bom.components.new(
         name="s2",
-        description="SN2, SN3 Chassis sm tower, gigabyte motherboard, eur pricing",
-        cost=445,
+        description="AR chassis, eur pricing",
+        cost=539,
         rackspace_u=0,
         cru=0,
         sru=0,
@@ -25,41 +25,65 @@ def bom_populate(bom):
         mru=0,
         su_perc=50,
         cu_perc=50,
-        power=150,
-    )
-
-    bom.components.new(name="margin", description="margin per node for threefold and its partners", power=0, cost=500)
-
-    bom.components.new(
-        name="hd12", description="Seagate 3.5'' 12TB SATA 6Gb/s 7.2K RPM 256MB 512e/4kn Helium", cost=276, hru=12000, power=10, su_perc=100
+        power=120,
     )
     
     bom.components.new(
+        name="s3",
+        description="Fractal case, Asrock M-ITX, eur pricing",
+        cost=841,
+        rackspace_u=0,
+        cru=0,
+        sru=0,
+        hru=0,
+        mru=0,
+        su_perc=50,
+        cu_perc=50,
+        power=120,
+    )
+   
+    bom.components.new(name="margin", description="margin per node for threefold and its partners", power=0, cost=500)
+
+    bom.components.new(name="hd4", description="Seagate 3.5'' 4TB SATA Barracuda", cost=95, hru=4000, power=10, su_perc=100)
+    
+    bom.components.new(name="hd2", description="Seagate 2.5'' 2TB SATA Barracuda", cost=80, hru=2000, power=10, su_perc=100)
+    
+    bom.components.new(
         name="amd1",
-        description="AMD Ryzen 3 2200G 3,5Gz 4-Cores Socket AM4 + Cooler (4 logical cores)",
-        cost=95,
-        cru=4,
+        description="AMD Ryzen 5 3400GE 3,4Gz 6-Cores Socket AM4 + Cooler (8 logical cores)",
+        cost=145,
+        cru=8,
         power=80,
         cu_perc=100,
-        passmark=6797,
+        passmark=9407,
     )
     
     bom.components.new(
         name="amd2",
-        description="AMD Ryzen 7 2700 3,2Gz 8-Cores Socket AM4 + Cooler (16 logical cores)",
-        cost=296,
-        cru=16,
+        description="AMD Ryzen 5 3600 3,4Gz 6-Cores Socket AM4 + Cooler (12 logical cores)",
+        cost=180,
+        cru=12,
         power=80,
         cu_perc=100,
-        passmark=17601,
+        passmark=17790,
     )
     
-
-    bom.components.new(name="ssd1", description="Intel D1 P4101 256GB NVMe PCIe 3.0 x4 M.2 22x80mm  0.5DWPD", cost=82, sru=256, power=10, su_perc=100)
+    bom.components.new(
+        name="intel1",
+        description="Intel Core i5-5257U 2,7 GHz (4 logical cores)",
+        cost=0,
+        cru=4,
+        power=80,
+        cu_perc=100,
+        passmark=3269,
+    )
+        
     
-    bom.components.new(name="ssd2", description="Intel D1 P4101 1TB NVMe PCIe 3.0 x4 M.2 22x80mm 0.5DWPD", cost=292, sru=1000, power=10, cu_perc=100)
+    bom.components.new(name="ssd1", description="Intenso 512GB SSD 2.5", cost=70, sru=512, power=10, cu_perc=100)
     
-    bom.components.new(name="mem32_ecc", description="Samsung 32GB DDR4-2666 NON-ECC UNB 1Rx4", cost=110, mru=32, power=8, cu_perc=100)
+    bom.components.new(name="mem32_ecc", description="Samsung 2 x 16 DDR4", cost=150, mru=32, power=8, cu_perc=100)
+    
+    bom.components.new(name="mem64_ecc", description="Corsair 2 x 32 DDR4", cost=220, mru=64, power=8, cu_perc=100)
     
     bom.components.new(
         name="ng2",
@@ -69,55 +93,30 @@ def bom_populate(bom):
         rackspace_u=1,
     )
 
-    # create the template sn1
-    d = bom.devices.new(name="sn1")
+    # create the template small
+    d = bom.devices.new(name="small")
     d.components.new(name="s1", nr=1)
+    d.components.new(name="intel1", nr=1)
+    d.components.new(name="hd2", nr=1)
+    #d.components.new(name="mem32_ecc", nr=1)
+    #d.components.new(name="ssd1", nr=1)
+
+    # create the template medium
+    d = bom.devices.new(name="medium")
+    d.components.new(name="s2", nr=1)
     d.components.new(name="amd1", nr=1)
-    d.components.new(name="hd12", nr=0)
+    d.components.new(name="hd4", nr=1)
     d.components.new(name="mem32_ecc", nr=1)
     d.components.new(name="ssd1", nr=1)
-
-    # create the template sn2
-    d = bom.devices.new(name="sn2")
-    d.components.new(name="s2", nr=1)
-    d.components.new(name="amd2", nr=1)
-    d.components.new(name="hd12", nr=0)
-    d.components.new(name="mem32_ecc", nr=2)
-    d.components.new(name="ssd2", nr=1)
     
-    # create the template sn2hdd
-    d = bom.devices.new(name="sn2hdd")
-    d.components.new(name="s2", nr=1)
+    # create the template large
+    d = bom.devices.new(name="large")
+    d.components.new(name="s3", nr=1)
     d.components.new(name="amd2", nr=1)
-    d.components.new(name="hd12", nr=1)
-    d.components.new(name="mem32_ecc", nr=2)
+    d.components.new(name="hd4", nr=2)
+    d.components.new(name="mem64_ecc", nr=1)
     d.components.new(name="ssd2", nr=1)
      
-    # create the template sn3
-    d = bom.devices.new(name="sn3")
-    d.components.new(name="s2", nr=1)
-    d.components.new(name="amd2", nr=1)
-    d.components.new(name="hd12", nr=2)
-    d.components.new(name="mem32_ecc", nr=2)
-    d.components.new(name="ssd2", nr=1)
-    
-    # create the template sn4
-    d = bom.devices.new(name="sn4")
-    d.components.new(name="s2", nr=1)
-    d.components.new(name="amd2", nr=1)
-    d.components.new(name="hd12", nr=3)
-    d.components.new(name="mem32_ecc", nr=2)
-    d.components.new(name="ssd2", nr=1)
-    
-    # create the template sn5
-    d = bom.devices.new(name="sn5")
-    d.components.new(name="s2", nr=1)
-    d.components.new(name="amd2", nr=1)
-    d.components.new(name="hd12", nr=4)
-    d.components.new(name="mem32_ecc", nr=2)
-    d.components.new(name="ssd2", nr=1)
-
-
     d = bom.devices.new(name="switch_48")
     d.components.new(name="ng2", nr=1)
     
