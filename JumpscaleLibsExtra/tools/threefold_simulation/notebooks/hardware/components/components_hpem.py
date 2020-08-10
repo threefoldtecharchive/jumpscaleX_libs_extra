6c2a2ab7ed87677b3d8ca38f780a9eac9f2d4864
Weynand Kuijpers <weynandkuijpers@gmail.com>
2020-08-10 12:01:45 +0200

    Update components_hpem.py

--- JumpscaleLibsExtra/tools/threefold_simulation/notebooks/hardware/components/components_hpem.py
+++ JumpscaleLibsExtra/tools/threefold_simulation/notebooks/hardware/components/components_hpem.py
@@ -1,6 +1,20 @@
 def bom_populate(bom):
     
     bom.components.new(
+        name="s0",
+        description="HPE ProLiant MicroServer Gen10 Plus E-2224 4-core 1P 16GB-U S100i 4LFF-NHP 180W External PS Server",
+        cost=378,
+        rackspace_u=1,  # Rackspace unit set to 0 to mimic the perk to the tennant that needs to be included in the modeling
+        cru=0,
+        sru=0,
+        hru=0,
+        mru=0,
+        su_perc=50,
+        cu_perc=50,
+        power=150,
+    )
+    
+    bom.components.new(
         name="s1",
         description="HPE ML30 INTEL, 4U Tower, 8 HD's fit inside, 1 gbit dual",
         cost=378,
@@ -33,6 +47,9 @@ def bom_populate(bom):
     bom.components.new(
         name="hd12", description="HPE 12TB SATA 6G Midline 7.2K LFF (6-8 watt)", cost=350, hru=12000, power=10, su_perc=100
     )
+    bom.components.new(
+        name="hd4", description="HPE 4TB SATA 6G Midline 7.2K LFF (3.5in) RW 1yr Wty", cost=200, hru=4000, power=10, su_perc=100
+    )
     
     bom.components.new(
         name="intel1",
@@ -64,6 +81,7 @@ def bom_populate(bom):
         passmark=20656,
     )
 
+    bom.components.new(name="ssd240", description="240 GB HPE SSD", cost=120, sru=240, power=10, su_perc=100)
     bom.components.new(name="ssd1", description="960 GB HPE SSD", cost=180, sru=1920, power=10, su_perc=100)
     
     bom.components.new(name="mem16_ecc", description="mem 16", cost=98, mru=16, power=8, cu_perc=100)
@@ -85,7 +103,19 @@ def bom_populate(bom):
         power=100,
         rackspace_u=1,
     )
-
+    
+    # create the template microserver
+    d = bom.devices.new(name="hpe_microserver")
+    d.components.new(name="s0", nr=1)
+    d.components.new(name="intel1", nr=1)
+    d.components.new(name="hd4", nr=3)
+    d.components.new(name="mem16_ecc", nr=1)
+    d.components.new(name="ssd240", nr=1)
+    #d.components.new(name="sas_contr", nr=1)
+    #d.components.new(name="power_supply", nr=1)
+    #d.components.new(name="sas_cable", nr=1)
+    #d.components.new(name="front_fan", nr=1)
+    
     # create the template ml30
     d = bom.devices.new(name="hpe_compute_tower_ml30")
     d.components.new(name="s1", nr=1)
